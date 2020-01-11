1250d6732651e193a066ab3228c5de7265d3c1ba
Matías Fonzo <selk@dragora.org>
2020-01-10 21:16:50 -0300

    recipes: tde: make trinity visible for the system

--- archive/pkgconf/etc/profile.d/pkgconf.sh
+++ archive/pkgconf/etc/profile.d/pkgconf.sh
@@ -5,7 +5,7 @@ test -n "${PKG_CONFIG+$PKG_CONFIG}" || \
  PKG_CONFIG=/usr/bin/pkgconf
 
 test -n "${PKG_CONFIG_PATH+$PKG_CONFIG_PATH}" || \
- PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig
+ PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig:/opt/trinity/lib/pkgconfig
 
 export PKG_CONFIG PKG_CONFIG_PATH
 
