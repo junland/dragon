52b20f505f9b54baf7eba9427e93e2661c611ae9
Matías Fonzo <selk@dragora.org>
2020-05-27 11:35:44 -0300

    recipes: tools/coreutils: Upgraded to version 8.32
    Thanks to Thorsten Glaser for the tip on the -N option of ls(1).
    
    Signed-off-by: Matías Fonzo <selk@dragora.org>

--- archive/coreutils/etc/profile.d/dircolors.sh
+++ archive/coreutils/etc/profile.d/dircolors.sh
@@ -4,9 +4,9 @@ if type dircolors > /dev/null
 then
     if test -r /etc/DIR_COLORS
     then
-        eval "`dircolors -b /etc/DIR_COLORS`"
+        eval "$(dircolors -b /etc/DIR_COLORS)"
     else
-        eval "`dircolors -b`"
+        eval "$(dircolors -b)"
     fi
 fi
 
@@ -14,15 +14,20 @@ fi
 #
 # Options:
 #
-#    -b  Print C-style escapes for nongraphic characters
-#    -C  List entries by columns
-#    -l  Use a long listing format
-#    -T  Assume tab stops at each COLS instead of 8
+#    -b  Print C-style escapes for nongraphic characters.
+#    -C  List entries by columns.
+#    -l  Use a long listing format.
+#    -T  Assume tab stops at each COLS instead of 8.
+#    -N  Print entry names without quoting.
+#        (See https://bugs.debian.org/813164 for references)
+#    --color
+#        Colorize the output.
 #
+
 if test -n "$LS_COLORS"
 then
-    alias ls='ls -T 0 --color=auto'
-    alias dir='ls -C -b -T 0 --color=auto'
-    alias vdir='ls -l -b -T 0 --color=auto'
+    alias ls='ls -N -T 0 --color=auto'
+    alias dir='ls -N -C -b -T 0 --color=auto'
+    alias vdir='ls -N -l -b -T 0 --color=auto'
 fi
 
