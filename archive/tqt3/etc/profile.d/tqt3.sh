88114c718f54a6e51e78255f370a2886e8180014
Matías Fonzo <selk@dragora.org>
2021-01-21 11:04:07 -0300

    Continue with the upgrade, mostly at devel/ and other changes
    Signed-off-by: Matías Fonzo <selk@dragora.org>

--- archive/tqt3/etc/profile.d/tqt3.sh
+++ archive/tqt3/etc/profile.d/tqt3.sh
@@ -1,13 +1,13 @@
 
 # Set QTDIR if unset
 
-test -n "${QTDIR+$QTDIR}" || QTDIR=/opt/trinity/lib/tqt3
+test -n "${QTDIR+$QTDIR}" || QTDIR=/opt/trinity/lib/tqt3-trinity
 
 export QTDIR
 
 # Expand Trinity's binaries path (if not already added)
 
-if ! printf "%s\n" "$PATH" | grep -q /opt/trinity/bin
+if ! printf '%s\n' "$PATH" | grep -q /opt/trinity/bin
 then
     PATH="${PATH}:/opt/trinity/bin"
     export PATH
